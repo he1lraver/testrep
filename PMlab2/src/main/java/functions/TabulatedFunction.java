57f0d962e3ec8cc0bf21f488cc6d7480c9791556
PeVANoD <52381759+PeVANoD@users.noreply.github.com>
2023-10-07 17:41:45 +0400

    TabulatedFunction.java

--- PMlab2/src/main/java/functions/TabulatedFunction.java
+++ PMlab2/src/main/java/functions/TabulatedFunction.java
@@ -1,13 +1,12 @@
 package functions;
 
-public interface TabulatedFunction extends MathFunction{
+public interface TabulatedFunction extends MathFunction {
     int getCount();
     double getX(int index);
     double getY(int index);
-    void SetY(int index, double value);
+    void setY(int index, double value);
     int indexOfX(double x);
     int indexOfY(double y);
     double leftBound();
     double rightBound();
-
 }
