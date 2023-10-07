140417df3d365a8961b5f737be00596b2af0fdfe
he1lraver <145396859+he1lraver@users.noreply.github.com>
2023-10-07 15:00:36 +0400

    Add files via upload

---
+++ PMlab2/src/main/java/functions/UnitFunction.java
@@ -0,0 +1,6 @@
+package functions;
+public class UnitFunction implements MathFunction {
+    public double apply(double x) {
+        return 1;
+    }
+}
---
+++ PMlab2/src/main/java/functions/ZeroFunction.java
@@ -0,0 +1,9 @@
+package functions;
+
+public class ZeroFunction implements MathFunction
+{
+    public double apply(double x)
+    {
+        return 0;
+    }
+}
