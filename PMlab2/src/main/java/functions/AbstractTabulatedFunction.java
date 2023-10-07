a8359b42b4d37f6d5b1f9c4f8c1cf82ca303254e
he1lraver <145396859+he1lraver@users.noreply.github.com>
2023-10-07 16:23:17 +0400

    Add files via upload

---
+++ PMlab2/src/main/java/functions/AbstractTabulatedFunction.java
@@ -0,0 +1,27 @@
+package functions;
+
+public abstract class AbstractTabulatedFunction implements TabulatedFunction {
+    protected abstract int floorIndexOfX(double x);
+    protected abstract double extrapolateLeft(double x);
+    protected abstract double extrapolateRight(double x);
+    protected abstract double interpolate(double x, int floorIndex);
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY)
+    {
+        return leftY + (rightY - leftY) / (rightX - leftX) * (x - leftX);
+    }
+
+    @Override
+    public double apply(double x) {
+        int index = indexOfX(x);
+        if (index != -1) {
+            return getY(index);
+        } else if (x < getX(0)) {
+            return extrapolateLeft(x);
+        } else if (x > getX(getCount() - 1)) {
+            return extrapolateRight(x);
+        } else {
+            int floorIndex = floorIndexOfX(x);
+            return interpolate(x, floorIndex);
+        }
+    }
+}
