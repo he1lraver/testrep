d1514e0adb84a19246d74a3640d6e121fae59fe2
he1lraver <145396859+he1lraver@users.noreply.github.com>
2023-10-07 14:28:15 +0400

    Add files via upload

---
+++ PMlab2/src/main/java/functions/ConstantFunction.java
@@ -0,0 +1,17 @@
+package functions;
+public class ConstantFunction implements MathFunction {
+    private final double constant;
+
+    public ConstantFunction(double constant) {
+        this.constant = constant;
+    }
+
+    public double getConstant() {
+        return constant;
+    }
+
+    @Override
+    public double apply(double x) {
+        return constant;
+    }
+}
