700401c9d17ba33ee5b329ecfbe3f3b50ccd5c02
he1lraver <145396859+he1lraver@users.noreply.github.com>
2023-10-07 14:58:57 +0400

    CompositeFunctionTest.java
    add new tests

--- PMlab2/src/main/java/functions/CompositeFunctionTest.java
+++ PMlab2/src/main/java/functions/CompositeFunctionTest.java
@@ -3,12 +3,15 @@ import org.junit.Test;
 import junit.framework.Assert;
 public class CompositeFunctionTest {
 
-    MathFunction lnFunc = new LnFunction();
+    MathFunction cos2xFunc = new Cos2xFunction();
     MathFunction sqrFunc = new SqrFunction();
-    MathFunction test3 = new CompositeFunction(sqrFunc, lnFunc);
+    MathFunction unitFunc = new UnitFunction();
+    MathFunction test3 = new CompositeFunction(sqrFunc, cos2xFunc);
+    MathFunction test4 = new CompositeFunction(cos2xFunc, unitFunc);
     @Test
     public void TestEquals(){
-        Assert.assertEquals(1.0, test3.apply(1));
+        Assert.assertEquals(0.1, test3.apply(0));
+        Assert.assertEquals(1.1, test4.apply(0));
     }
 
 }
