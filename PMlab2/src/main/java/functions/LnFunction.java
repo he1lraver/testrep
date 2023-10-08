8c6758f5a25ea1ef9f1c0163c22e487798bb29fe
PeVANoD <52381759+PeVANoD@users.noreply.github.com>
2023-10-08 15:11:17 +0400

    Delete PMlab2/src/main/java/functions directory

--- PMlab2/src/main/java/functions/LnFunction.java
+++
@@ -1,7 +0,0 @@
-package functions;
-
-class LnFunction implements MathFunction{
-    public double apply(double x){
-        return Math.log(x);
-    }
-}
--- PMlab2/src/main/java/functions/UnitFunction.java
+++
@@ -1,6 +0,0 @@
-package functions;
-public class UnitFunction implements MathFunction {
-    public double apply(double x) {
-        return 1;
-    }
-}
